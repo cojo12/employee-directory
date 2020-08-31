0130da7e643b8eb7982921a6197cb008c66829da
Cole Joko <joko.cole.cj@gmail.com>
2020-08-31 14:26:59 -0600

    added util folder and header/index.js

---
+++ client/src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header, Icon, Divider } from 'semantic-ui-react'
+
+const HeaderUsers = () => (
+  <div>
+    <Divider hidden />
+    <Header color='teal'as='h2' icon textAlign='center'>
+    <Icon circular inverted color='teal' name='users'/>
+    </Header>
+    <Divider segment />
+  </div>
+)
+
+export default HeaderUsers
