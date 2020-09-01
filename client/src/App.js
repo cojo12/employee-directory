347c0f7c0d50f085bfe1aca60eed69c3f71641d1
Cole Joko <joko.cole.cj@gmail.com>
2020-09-01 13:50:21 -0600

    updated dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import HeaderUsers from "./components/header/index";
+import EmployeeData from "./components/table/employeeData"
 import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import { Container, Divider } from "semantic-ui-react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HeaderUsers />
+      <Divider hidden />
+      <Container>
+        <EmployeeData/>
+      </Container>
     </div>
   );
+  
 }
 
 export default App;
