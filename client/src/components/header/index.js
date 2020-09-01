a5d3e9ab7b530cb75229951e142467ba11b70de0
Cole Joko <joko.cole.cj@gmail.com>
2020-09-01 14:18:48 -0600

    updated header index

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -4,8 +4,8 @@ import { Header, Icon, Divider } from 'semantic-ui-react'
 const HeaderUsers = () => (
   <div>
     <Divider hidden />
-    <Header color='teal'as='h2' icon textAlign='center'>
-    <Icon circular inverted color='teal' name='users'/>
+    <Header color='black'as='h2' icon textAlign='center'>
+    <Icon circular inverted color='white' name='users'/>
     </Header>
     <Divider segment />
   </div>
